7d2ee2321ba0386489d42b32634209c4c1740786
Henrique Gonçalves <henrique@MacBook-Pro-de-Henrique.local>
2023-03-17 06:04:42 +0000

    First phase of frontend done for zkconnect

--- app/src/app/page.tsx
+++ app/src/app/page.tsx
@@ -1,24 +1,74 @@
 'use client';
 
+import { useEffect, useState } from "react";
 import Image from 'next/image'
 import { Inter } from 'next/font/google'
 import styles from './page.module.css'
 
-import { ZkConnect, ZkConnectClientConfig } from "@sismo-core/zk-connect-client";
-import { zkConnectBackend } from "./zkconnect-backend";
+import {
+  DataRequest,
+  ZkConnect,
+  ZkConnectClientConfig,
+  ZkConnectResponse,
+} from "@sismo-core/zk-connect-client";
+
 import { useRouter } from 'next/router';
+
+const zkConnectConfig: ZkConnectClientConfig = {
+  appId: "0x52913711b4d9d877a522b06170b5648f"
+};
+const zkConnect = ZkConnect(zkConnectConfig);
+
+const THE_ETH_RICH_USERS = DataRequest({
+  groupId: "0x42c768bb8ae79e4c5c05d3b51a4ec74a", // TODO: change this id to our group id created by Tom when PR merged
+});
 
 const inter = Inter({ subsets: ['latin'] })
 
 export default function Home() {
-  const router = useRouter();
-  console.log(router.query)
-  const loginZk = async () => {
-    // Now that you have the proof, you need to verify it in your backend to be sure that the proof is valid. If yes, you will allow your user to access your application.
-    if(!zkConnectResponse) return;
-    const { vaultId } = await zkConnectBackend.verify(zkConnectResponse);
+  const [verifying, setVerifying] = useState(false);
+  const [zkConnectResponse, setZkConnectResponse] =
+    useState<ZkConnectResponse | null>(null);
 
+  function onZkConnectButtonClick() { 
+    // user gets redirected to get proof on data vault
+    zkConnect.request({
+      dataRequest: THE_ETH_RICH_USERS,
+    });
   }
+
+  // const router = useRouter();
+  // console.log(router.query)
+  // const loginZk = async () => {
+  //   // Now that you have the proof, you need to verify it in your backend to be sure that the proof is valid. If yes, you will allow your user to access your application.
+  //   if(!zkConnectResponse) return;
+  //   const { vaultId } = await zkConnectBackend.verify(zkConnectResponse);
+  // }
+
+  useEffect(() => {
+    const zkConnectResponse = zkConnect.getResponse();
+    if (zkConnectResponse) { // when user gets redirected to our app
+      setZkConnectResponse(zkConnectResponse);
+      setVerifying(true);
+
+      // If the proof is verified, a vaultId is returned. If not, an error is received.
+      fetch('/api/verify', {
+        method: 'POST',
+        headers: { 'Content-Type': 'application/json' },
+        body: JSON.stringify({
+          zkConnectResponse
+        }),
+      }).then(res => {
+        setVerifying(false);
+        // SUCCESS REDIRECT TO AUCTION PAGE
+      })
+      .catch(err => {
+        console.log(err.response.data.status);
+        setVerifying(false);
+      });
+    }
+  }, []);
+
   return (
     <main className={styles.main}>
       <div className={styles.description}>
@@ -46,17 +96,17 @@ export default function Home() {
       </div>
 
       <div className={styles.center}>
-        <Image
-          className={styles.logo}
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-        <div className={styles.thirteen}>
-          <Image src="/thirteen.svg" alt="13" width={40} height={31} priority />
-        </div>
+        <button
+          className={styles.thirteen}
+          onClick={onZkConnectButtonClick}
+          disabled={verifying}
+        >
+          {verifying ? (
+              <span>verifying...</span>
+          ) : (
+              <span>zkConnect</span>
+          )}
+        </button>
       </div>
 
       <div className={styles.grid}>
@@ -103,19 +153,3 @@ export default function Home() {
     </main>
   )
 }
-
-
-const zkConnectConfig: ZkConnectClientConfig = {
-  appId: "0x52913711b4d9d877a522b06170b5648f", // appId you registered
-}
-
-const zkConnect = ZkConnect(zkConnectConfig);
-
-zkConnect.request();
-
-// user gets redirected to prove his data vault
-
-const zkConnectResponse = zkConnect.getResponse(); // when he gets redirected to our app
-
-
-// If the proof is valid, a vaultId is returned. If not, an error is received.
